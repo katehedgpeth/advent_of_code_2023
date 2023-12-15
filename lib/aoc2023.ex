806a9ab741780093968e61ffb9e5aa3ccae043df
Kate Hedgpeth <katehedgpeth@gmail.com>
2023-12-15 16:20:12 -0500

    add file reader functions

--- lib/aoc2023.ex
+++ lib/aoc2023.ex
@@ -15,4 +15,40 @@ defmodule Aoc2023 do
   def hello do
     :world
   end
+
+  @doc """
+  Reads a file and splits it into lines. (splitter defaults to "\n")
+  Uses Module.underscore/1 to generate file path based on module name,
+  so folders in priv should not use underscores before numbers - i.e.,
+  Aoc2023.Day1 files should be put in "priv/day1/*"
+
+  ## Examples
+      iex> Aoc2023.read_input_file(Aoc2023.Day1, :test)
+      [ "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" ]
+  """
+
+  def read_input_file(module, input_type, splitter \\ "\n") do
+    :aoc_2023
+    |> :code.priv_dir()
+    |> Path.join(input_file_name(module, input_type))
+    |> File.read!()
+    |> String.split(splitter)
+  end
+
+  @doc """
+  Builds the path to a file based on a module name.
+
+  ## Examples
+      iex> Aoc2023.input_file_name(Aoc2023.Day1, :test)
+      "day1/test_input.txt"
+  """
+  @spec input_file_name(module(), :test | :real) :: String.t()
+  def input_file_name(module, input_type) do
+    [_ | child_module] = Module.split(module)
+
+    child_module
+    |> Module.concat()
+    |> Macro.underscore()
+    |> Path.join("#{input_type}_input.txt")
+  end
 end
