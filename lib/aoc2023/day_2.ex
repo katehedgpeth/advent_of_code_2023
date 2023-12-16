3d10f7bc48c7817ca33263ae04d297f4cd042396
Kate Hedgpeth <katehedgpeth@gmail.com>
2023-12-16 09:40:06 -0500

    Day 2, part 2

--- lib/aoc2023/day_2.ex
+++ lib/aoc2023/day_2.ex
@@ -1,12 +1,16 @@
-defmodule Aoc2023.Day2 do
-  require Logger
+defmodule Aoc2023.Day2.Set do
+  defstruct red: 0, green: 0, blue: 0
+end
 
+defmodule Aoc2023.Day2 do
   @moduledoc """
   Day 2
   https://adventofcode.com/2023/day/2
   """
 
-  @max_counts %{
+  alias __MODULE__.Set
+
+  @max_counts %Set{
     red: 12,
     green: 13,
     blue: 14
@@ -34,7 +38,48 @@ defmodule Aoc2023.Day2 do
 
   """
   def part_1(input_type) do
-    parse_file(input_type)
+    input_type
+    |> parse_into_sets()
+    |> Stream.filter(&valid_game?/1)
+    |> Enum.reduce(0, &add_valid_ids/2)
+  end
+
+  @doc """
+  Part 2
+
+  ```
+  The power of a set of cubes is equal to the numbers of red, green,
+  and blue cubes multiplied together. The power of the minimum set of
+  cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36,
+  respectively. Adding up these five powers produces the sum 2286.
+
+  For each game, find the minimum set of cubes that must have been present.
+  What is the sum of the power of these sets?
+  ```
+
+  Strategy:
+  I altered the parsers a bit to make them reusable.
+
+  For this part, we want to find the largest number for each color
+  within a game, so we iterate over all the sets in a game with a simple
+  reducer. If any color in a set is larger than the current highest count
+  in the accumulator, update the accumulator. Then we simply multiply the
+  accumulated values for each game to get the power, and add the power of
+  each line.
+
+  iex> Aoc2023.Day2.part_2(:test)
+  2286
+
+  iex> Aoc2023.Day2.part_2(:real)
+  67335
+
+  """
+  def part_2(input_type) do
+    input_type
+    |> parse_into_sets()
+    |> Stream.map(&get_minimum_set/1)
+    |> Stream.map(&(&1.red * &1.green * &1.blue))
+    |> Enum.reduce(0, &Kernel.+/2)
   end
 
   #########################################################
@@ -52,26 +97,43 @@ defmodule Aoc2023.Day2 do
   ##
   #########################################################
 
-  defp parse_file(input_type) do
+  defp parse_into_sets(input_type) do
     __MODULE__
     |> Aoc2023.read_input_file(input_type)
-    |> Stream.map(&parse_line/1)
-    |> Enum.reduce(0, &Kernel.+/2)
+    |> Stream.map(&parse_line_into_sets/1)
   end
 
-  defp parse_line(<<"Game ", line::binary>>) do
-    [id, cube_sets] = String.split(line, ": ")
+  defp parse_line_into_sets(<<"Game ", line::binary>>) do
+    [id, sets] = String.split(line, ": ")
+    {id, String.split(sets, "; ")}
+  end
 
-    cube_sets
-    |> String.split("; ")
-    |> Enum.all?(&valid_set?/1)
-    |> case do
-      true ->
-        String.to_integer(id)
+  defp add_valid_ids({id, _}, acc) do
+    String.to_integer(id) + acc
+  end
 
-      false ->
-        0
-    end
+  defp get_minimum_set({_id, sets}) do
+    sets
+    |> Enum.map(&String.split(&1, ", "))
+    |> Enum.reduce(%Set{}, &reduce_minimum_sets/2)
+  end
+
+  defp reduce_minimum_sets(set, acc) do
+    set
+    |> Enum.map(&parse_cube/1)
+    |> Enum.reduce(acc, &update_minimum_color/2)
+  end
+
+  defp update_minimum_color(%{count: count, color: color}, acc) do
+    Map.update!(acc, color, fn old_count ->
+      if old_count > count,
+        do: old_count,
+        else: count
+    end)
+  end
+
+  defp valid_game?({_id, sets}) do
+    Enum.all?(sets, &valid_set?/1)
   end
 
   defp valid_set?(set) do
@@ -81,10 +143,16 @@ defmodule Aoc2023.Day2 do
   end
 
   defp valid_cube?(cube) do
-    [count, color] = String.split(cube, " ")
-    count = String.to_integer(count)
-    color = String.to_existing_atom(color)
-
+    %{count: count, color: color} = parse_cube(cube)
     Map.fetch!(@max_counts, color) >= count
+  end
+
+  defp parse_cube(cube) do
+    [count, color] = String.split(cube, " ")
+
+    %{
+      count: String.to_integer(count),
+      color: String.to_existing_atom(color)
+    }
   end
 end
