09c73a296d437b417c49caf63b64ec976f5fce72
Kate Hedgpeth <katehedgpeth@gmail.com>
2023-12-29 14:06:46 -0500

    Day 10, part 2

--- lib/aoc2023/day_10.ex
+++ lib/aoc2023/day_10.ex
@@ -1,5 +1,7 @@
 defmodule Aoc2023.Day10 do
   alias __MODULE__.Matrix
+  require Matrix
+  require Logger
 
   @doc """
   We are parsing a graph to determine the length of a loop.
@@ -42,27 +44,89 @@ defmodule Aoc2023.Day10 do
     {:ok, matrix} = Matrix.start_link(input_type: input_type)
 
     matrix
-    |> Matrix.get_start()
-    |> walk_loop(matrix)
-    |> length()
+    |> Matrix.boundary_point_count()
     |> Integer.floor_div(2)
   end
 
-  defp walk_loop({x, y}, matrix) do
-    walk([:north, :south, :east, :west], {x, y}, matrix, [])
+  @doc """
+  Now we are being asked to find the number of points that
+  are fully enclosed by the loop (i.e., not including the
+  boundary points).
+
+  Not gonna lie, this took me way, way, way longer than
+  these few lines of code make it seem. I eventually broke
+  down and looked at solutions on Reddit - even after that,
+  my application of shoelace and pick's kept giving me the
+  wrong number. I eventually had to break things down and
+  write tests for every atomic piece of the pipeline to
+  figure out where the calculations were going sideways.
+  (I've deleted those tests.)
+
+  iex> Aoc2023.Day10.part_2(:test)
+  1
+
+  iex> Aoc2023.Day10.part_2(:test2)
+  8
+
+  iex> Aoc2023.Day10.part_2(:real)
+  349
+  """
+  def part_2(input_type) do
+    {:ok, matrix} = Matrix.start_link(input_type: input_type)
+
+    matrix
+    |> Matrix.get_loop()
+    |> calculate_area(0)
+    |> calculate_inner_points(Matrix.boundary_point_count(matrix))
   end
 
-  defp walk([:start], _, _matrix, breadcrumbs) do
-    Enum.reverse(breadcrumbs)
+  @doc """
+  Uses the Shoelace formula to calculate the area of a
+  polygon with integer coordinates:
+
+         |x1   x2|                 |x2   x3|
+  2A =   |   X   |          +      |   X   |
+         |y1   y2|                 |y2   y3|
+
+  ((x1 * y2) - (x2 * y1)) + ((x2 * y3) - (x3 * y2))
+  """
+
+  def calculate_area([next, next_1 | rest], acc) do
+    calculate_area(
+      [next_1 | rest],
+      shoelace_acc(next, next_1, acc)
+    )
   end
 
-  defp walk([direction | rest], {x, y}, matrix, breadcrumbs) do
-    case Matrix.check_connection(matrix, from: {x, y}, direction: direction) do
-      {:error, _error} ->
-        walk(rest, {x, y}, matrix, breadcrumbs)
+  def calculate_area([_last], area) do
+    area
+    |> Integer.floor_div(2)
+    |> abs()
+  end
 
-      {:ok, {{new_x, new_y}, new_direction}} ->
-        walk([new_direction], {new_x, new_y}, matrix, [{x, y} | breadcrumbs])
-    end
+  defp shoelace_acc({x1, y1}, {x2, y2}, acc) do
+    left = x1 * y2
+    right = x2 * y1
+
+    acc + left - right
+  end
+
+  @doc """
+    Based on Pick's theorem to calculate the area of a polygon:
+    A = i + (b / 2) - 1
+
+    variables:
+    A = area
+    i = count of inner points
+    b = count of boundary points
+
+
+    We know A and b, so we change the formula to get i:
+
+    A + 1 = i + (b / 2)
+    A + 1 - (b / 2) = i
+  """
+  def calculate_inner_points(area, boundary_points) do
+    area + 1 - Integer.floor_div(boundary_points, 2)
   end
 end
--- lib/aoc2023/day_10/matrix.ex
+++ lib/aoc2023/day_10/matrix.ex
@@ -1,6 +1,16 @@
 defmodule Aoc2023.Day10.Matrix do
   use Agent
   alias Aoc2023.Day10
+  require Logger
+
+  @type direction() :: :east | :north | :south | :west
+  @type coords() :: {integer(), integer()}
+  @type symbol() :: {direction(), direction()}
+  @type matrix() :: %{
+          integer() => %{
+            integer() => symbol()
+          }
+        }
 
   @symbol_map %{
     "|" => {:north, :south},
@@ -10,11 +20,12 @@ defmodule Aoc2023.Day10.Matrix do
     "7" => {:south, :west},
     "F" => {:south, :east},
     "." => :ground,
+    "I" => :ground,
     "S" => :start
   }
 
-  @type direction() :: :east | :north | :south | :west
-  @type coords() :: {integer(), integer()}
+  @reverse_symbols @symbol_map
+                   |> Map.new(&{elem(&1, 1), elem(&1, 0)})
 
   @symbols Map.values(@symbol_map)
 
@@ -23,27 +34,101 @@ defmodule Aoc2023.Day10.Matrix do
   end
 
   def init(input_type: input_type) do
-    matrix = parse_matrix(input_type)
+    init(stream: Aoc2023.read_input_file(Day10, input_type))
+  end
+
+  def init(stream: stream) do
+    matrix = parse_matrix(stream)
     start = find_start(matrix)
 
     %{
       matrix: matrix,
-      start: start
+      start: start,
+      loop: walk_loop(%{matrix: matrix, start: start}),
+      inners: count_inner_pieces(stream)
     }
+  end
+
+  def get_loop(pid) do
+    Agent.get(pid, & &1.loop)
+  end
+
+  @doc """
+  Returns the number of boundary points in the loop.
+  The loop stored in the agent includes the start point twice
+  (first and last) in order to correctly calculate the area
+  with the shoelace formula. So, the actual count is
+  length(loop) - 1.
+  """
+  def boundary_point_count(pid) when is_pid(pid) do
+    Agent.get(pid, &(length(&1.loop) - 1))
+  end
+
+  def print(pid) do
+    loop =
+      pid
+      |> walk_loop()
+      |> MapSet.new()
+
+    pid
+    |> Agent.get(& &1.matrix)
+    |> Enum.map(&print_line(&1, loop))
+    |> Enum.join("")
+  end
+
+  defp print_line({y, line}, loop) do
+    "\n" <>
+      (line
+       |> Enum.map(&print_char(&1, y, loop))
+       |> Enum.join(""))
+  end
+
+  defp print_char({x, symbol}, y, loop) do
+    if MapSet.member?(loop, {x, y}),
+      do: Map.fetch!(@reverse_symbols, symbol),
+      else: " "
+  end
+
+  defp count_inner_pieces(stream) do
+    Enum.reduce(stream, 0, &_count_inner_pieces/2)
+  end
+
+  defp _count_inner_pieces(line, acc) do
+    String.replace(line, ~r/[^I]/, "")
+    |> String.length()
+    |> Kernel.+(acc)
   end
 
   def get_start(pid) do
     Agent.get(pid, &Map.fetch!(&1, :start))
   end
 
+  def get_inner_count(pid) do
+    Agent.get(pid, &Map.fetch!(&1, :inners))
+  end
+
   def get_cell(pid, {x, y}) do
-    Agent.get(pid, &_get_cell(&1, {x, y}))
+    Agent.get(pid, &_get_cell(&1.matrix, {x, y}))
+  end
+
+  def get_cell!(pid, {x, y}) do
+    case get_cell(pid, {x, y}) do
+      {:ok, cell} -> cell
+      :error -> raise "cell {#{x}, #{y}} not found in matrix"
+    end
+  end
+
+  def get_line!(pid, y) do
+    Agent.get(pid, &(&1 |> Map.fetch!(:matrix) |> Map.fetch!(y)))
   end
 
   def go(:north, {x, y}), do: {x, y - 1}
   def go(:south, {x, y}), do: {x, y + 1}
   def go(:west, {x, y}), do: {x - 1, y}
   def go(:east, {x, y}), do: {x + 1, y}
+
+  def x({x, y}) when is_integer(x) and is_integer(y), do: x
+  def y({x, y}) when is_integer(x) and is_integer(y), do: y
 
   def opposite_direction(direction) do
     case direction do
@@ -54,17 +139,23 @@ defmodule Aoc2023.Day10.Matrix do
     end
   end
 
-  @spec check_connection(pid(), [
+  @spec check_connection(pid() | matrix(), [
           {:direction, direction()} | {:from, {any(), any()}}
         ]) ::
           {:error, :edge | :ground | {:no_match, any()}}
           | {:ok, {coords(), direction() | :start}}
-  def check_connection(pid, from: from, direction: direction) do
+  def check_connection(pid, opts) when is_pid(pid) do
+    pid
+    |> Agent.get(& &1)
+    |> check_connection(opts)
+  end
+
+  def check_connection(%{0 => _} = matrix, from: from, direction: direction) do
     to = go(direction, from)
 
     from_direction = opposite_direction(direction)
 
-    case get_cell(pid, to) do
+    case _get_cell(matrix, to) do
       :error ->
         {:error, :edge}
 
@@ -85,7 +176,7 @@ defmodule Aoc2023.Day10.Matrix do
     end
   end
 
-  defp _get_cell(%{matrix: matrix}, {x, y}) do
+  defp _get_cell(%{0 => _} = matrix, {x, y}) do
     with {:ok, row} <- Map.fetch(matrix, y) do
       Map.fetch(row, x)
     end
@@ -112,9 +203,8 @@ defmodule Aoc2023.Day10.Matrix do
     symbol == :start
   end
 
-  def parse_matrix(input_type) do
-    Day10
-    |> Aoc2023.read_input_file(input_type)
+  def parse_matrix(stream) do
+    stream
     |> Stream.map(&parse_line/1)
     |> Stream.with_index()
     |> Stream.map(&reverse_idx/1)
@@ -133,4 +223,33 @@ defmodule Aoc2023.Day10.Matrix do
   end
 
   defp reverse_idx({val, idx}), do: {idx, val}
+
+  defp walk_loop(matrix) when is_pid(matrix) do
+    matrix
+    |> Agent.get(& &1)
+    |> walk_loop()
+  end
+
+  defp walk_loop(%{start: {x, y}, matrix: matrix}) do
+    walk([:north, :east, :south, :west], {x, y}, matrix, [])
+  end
+
+  defp walk([:start], {x, y}, _matrix, breadcrumbs) do
+    [{x, y} | breadcrumbs]
+  end
+
+  defp walk([direction | rest], {x, y}, matrix, breadcrumbs) do
+    case check_connection(matrix, from: {x, y}, direction: direction) do
+      {:error, _error} ->
+        walk(rest, {x, y}, matrix, breadcrumbs)
+
+      {:ok, {{new_x, new_y}, new_direction}} ->
+        walk(
+          [new_direction],
+          {new_x, new_y},
+          matrix,
+          [{x, y} | breadcrumbs]
+        )
+    end
+  end
 end
